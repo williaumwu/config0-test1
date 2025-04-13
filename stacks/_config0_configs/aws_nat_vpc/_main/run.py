8f29fc3ea1e99b63d81e38742b51c6ec5da8d4cf
gary <gary>
2025-04-13 08:17:41 +0800

    'udpate'

--- stacks/_config0_configs/aws_nat_vpc/_main/run.py
+++ stacks/_config0_configs/aws_nat_vpc/_main/run.py
@@ -42,8 +42,8 @@ def run(stackargs):
                              types="str")
 
 
-    stack.add_execgroup("williaumwu:::config0-test1::sg_3tier")
-    stack.add_substack('williaumwu:::tf_executor')
+    stack.add_execgroup("williaumwu:::config0-test1::natgw_vpc_broke")
+    #stack.add_substack('williaumwu:::tf_executor')
 
     # Initialize
     stack.init_variables()
